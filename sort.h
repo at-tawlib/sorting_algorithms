b348fea599098d44a3027697ed82b9b71f69d48f
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-23 22:10:32 +0000

    Rename swap

--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void swap(int *first, int *second);
+void swap_int(int *first, int *second);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
