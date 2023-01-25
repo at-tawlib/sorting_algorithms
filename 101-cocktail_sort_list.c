df7482d8793eff6c72e2998a2627790f453a5d2d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-25 05:46:01 +0000

    Create 101-cocktail_sort_list.c

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - orts a doubly linked list of integers in ascending
+ * order using the Cocktail shaker sort algorithm
+ *
+ * @list: list to sort
+ */
+void cocktail_sort_list(listint_t **list)
+{
+
+}
