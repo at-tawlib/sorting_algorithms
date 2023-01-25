5f37f6f281e81f3f4784ea872601dd896206531a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-25 05:32:56 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
 
 #endif
