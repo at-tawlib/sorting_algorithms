b264b153c2e1c24ee41388348b88a7374b8e9513
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-28 22:06:27 +0000

    Add cocktail_sort and associtate functions

--- sort.h
+++ sort.h
@@ -27,7 +27,9 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
-void cocktail_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 
 #endif
