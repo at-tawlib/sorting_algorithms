2edac9c6dd5ab4a81cca64c9f97ba831dee3c6ed
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-28 18:08:59 +0000

    Add shell sort function

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void cocktail_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 
 #endif
